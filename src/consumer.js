9d3822b26c3540d06da93413036d8444fe6c2f9e
Wirayuda <yudaardi01@gmail.com>
2025-07-02 00:06:26 +0700

    troubleshoot error2

--- src/consumer.js
+++ src/consumer.js
@@ -4,12 +4,17 @@ const NotesService = require('./NotesService');
 const MailSender = require('./MailSender');
 const Listener = require('./listener');
 
+console.log('RABBITMQ_SERVER', process.env.RABBITMQ_SERVER);
+
 const init = async () => {
   const noteService = new NotesService();
   const mailSender = new MailSender();
   const listener = new Listener(noteService, mailSender);
 
-  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+  // const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+
+  const url = process.env.RABBITMQ_SERVER;
+  const connection = await amqp.connect(url, { servername: new URL(url).hostname });
   const channel = await connection.createChannel();
 
   await channel.assertQueue('export:notes', {
